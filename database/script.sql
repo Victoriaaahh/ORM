02d63a694ce7dda8c551c5801412b3c695ed400d
Victoria <victoriaayumikose@gail.com>
2023-11-16 11:26:48 -0300

    16/11

---
+++ database/script.sql
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    usuario = models.CharField(max_length=100, unique=True)
+    senha = models.CharField(max_length=60)
+    email = models.EmailField(max_length=255)
+    apresentacao = models.CharField(max_length=255, null=True)
+
+class Postagem(models.Model):
+    id = models.AutoField(primary_key=True)
+    conteudo = models.CharField(max_length=140)
+    data = models.DateTimeField(default=models.now)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+class Comentario(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE)
+    conteudo = models.CharField(max_length=140)
+    data = models.DateTimeField(default=models.now)
+
+class Reacao(models.Model):
+    postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    data = models.DateTimeField(default=models.now)
+
+    class Meta:
+        unique_together = ('postagem', 'usuario')
---
+++ scripts/1.py
@@ -0,0 +1,24 @@
+import os, sys, django
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django.setup()
+from app.models import Usuario
+
+usuarios = [
+    Usuario(nome='Pedro Santos', usuario='pedro789', senha='P!789senha',
+    email='pedro.santos@hotmail.com', apresentacao='E aí, eu sou o Pedro. Adoro
+    esportes e estou sempre em movimento.'),
+    Usuario(nome='Ana Pereira', usuario='ana101', senha='Ana*101Senha',
+    email='ana.pereira@gmail.com', apresentacao='Oi gente, eu sou a Ana. Sou
+    apaixonada por arte e música.'),
+    Usuario(nome='Lucas Oliveira', usuario='lucas202', senha='L@ucas202',
+    email='lucas.oliveira@gmail.com', apresentacao='Fala galera, eu sou o Lucas.
+    Amo tecnologia e programação.'),
+    Usuario(nome='Gabriela Lima', usuario='gabi303', senha='Gabi@303',
+    email='gabi.lima@yahoo.com', apresentacao='Oi, meu nome é Gabriela. Adoro
+    livros e café.'),
+    Usuario(nome='Carlos Pereira', usuario='carlos404', senha='Car!os404',
+    email='carlos.pereira@hotmail.com', apresentacao='E aí, eu sou o Carlos. Sou
+    fã de esportes radicais e aventuras.'),
+]
+Usuario.objects.bulk_create(usuarios)
